1c9739f1f957f0728f2d730c1f71fb1dbeeb1a93
shashwatniranjan-max <shashwatniranjan1@gmail.com>
2026-02-05 17:02:25 +0530

    solved codeforces 71A problem

--- Codeforces/4A.cpp
+++ Codeforces/4A.cpp
@@ -26,8 +26,9 @@ For example, the boys can divide the watermelon into two parts of 2 and 6 kilos 
 
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int n;
     cin >> n;
     if(n > 2 && n % 2 == 0) cout << "YES";
